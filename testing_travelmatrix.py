a3720a8d870a92597ba80950cebd1a99db9ba102
hilvif <hilfjo@gmail.com>
2024-02-09 15:14:25 +0100

    T_ij ferdig

--- testing_travelmatrix.py
+++ testing_travelmatrix.py
@@ -13,8 +13,8 @@ df_visits = pd.read_csv("data/test_data/VisitsNY.csv")
 
 def travel_matrix(df):
         # Radians distance matrix
-        rad_lat = [math.radians(float(i.replace(",", "").split()[0])) for i in df["location"]]
-        rad_long = [math.radians(float(i.replace(",", "").split()[1])) for i in df["location"]]
+        rad_lat = [math.radians(float(i.replace(",", "").replace("(", "").replace(")", "").split()[0])) for i in df["location"]]
+        rad_long = [math.radians(float(i.replace(",", "").replace("(", "").replace(")", "").split()[1])) for i in df["location"]]
         rad_location =list(zip(rad_lat ,rad_long))
         D_ij = haversine_distances(rad_location, rad_location) * 6371
         
@@ -28,20 +28,21 @@ def travel_matrix(df):
         for i in range(len(D_ij)):
             for j in range(len(D_ij)):
                 T_ij[i][j] = (timedelta(hours=(D_ij[i][j] / speed)).total_seconds() / 60)
-        print("T", T_ij)
-
+    
         # Rush hour modelling:
+        #TODO: Se på om vi må gjøre dette annerledes og generere rush hour factor basert på tiden som faktisk settes i heuristikken
         for k in range(len(T_ij)):
-            average_start_time = df.iloc[k][""]
-            if "starting time is between 7 or 9"
-            if "starting time is between 15 and 17"
-            df.iloc[k]["Requested Pickup Time"].hour >= 15 and df.iloc[k]["Requested Pickup Time"].hour < 17 and df.iloc[l]["Requested Pickup Time"].hour >= 15 and df.iloc[l]["Requested Pickup Time"].hour < 17:
-                T_ij[k][l] = T_ij[k][l]*rush_factor
-                T_ij[k+self.n][l] = T_ij[k+self.n][l]*rush_factor
-                T_ij[k][l+self.n] = T_ij[k][l+self.n]*rush_factor
-                T_ij[k+self.n][l+self.n] = T_ij[k+self.n][l+self.n]*rush_factor
-               
-
+            average_start_time = (df.iloc[k]["earliestStartTime"] + df.iloc[k]["latestStartTime"]) / 2
+            print("averagestarttime", average_start_time)
+            # If start time is between 07 and 09. 
+            if (df.iloc[k]["earliestStartTime"] >= 420 and df.iloc[k]["earliestStartTime"] <= 540) or (df.iloc[k]["latestStartTime"] >= 420 and df.iloc[k]["latestStartTime"] <= 540):
+                for l in range(len(T_ij)):
+                    print("old", T_ij[k][l], "new", T_ij[k][l]*rush_factor)
+                    T_ij[k][l] = T_ij[k][l]*rush_factor
+            # If start time is between 15 and 17.
+            if (df.iloc[k]["earliestStartTime"] >= 900 and df.iloc[k]["earliestStartTime"] <= 1020) or (df.iloc[k]["latestStartTime"] >= 900 and df.iloc[k]["latestStartTime"] <= 1020):
+                for l in range(len(T_ij)):
+                    T_ij[k][l] = T_ij[k][l]*rush_factor
         return T_ij
 
 matrisa = travel_matrix(df_nodes)
