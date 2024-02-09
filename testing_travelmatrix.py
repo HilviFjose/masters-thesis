6440daca8194f82c52e785dd158281214e52572a
hilvif <hilfjo@gmail.com>
2024-02-09 11:31:08 +0100

    travel matrix

--- testing_travelmatrix.py
+++ testing_travelmatrix.py
@@ -12,43 +12,37 @@ df_visits = pd.read_csv("data/test_data/VisitsNY.csv")
 
 
 def travel_matrix(df):
-        radians_latitude = [math.radians(float(i.replace(",", "").split()[0])) for i in df["location"]]
-        radians_longitude = [math.radians(float(i.replace(",", "").split()[1])) for i in df["location"]]
-        print("latlong", radians_latitude, radians_longitude)
-    
-       
-        #print("haversine", distances)
+        # Radians distance matrix
+        rad_lat = [math.radians(float(i.replace(",", "").split()[0])) for i in df["location"]]
+        rad_long = [math.radians(float(i.replace(",", "").split()[1])) for i in df["location"]]
+        rad_location =list(zip(rad_lat ,rad_long))
+        D_ij = haversine_distances(rad_location, rad_location) * 6371
+        
+        # TODO: De to under her bør inn i config etterhvert
+        # Buses in Oslo om average drive in 25 kms/h.
+        speed = 40
+        rush_factor = 2
 
-        # Distance matrix
+        # Travel time matrix - 2D-list with time given in minutes. 
+        T_ij = [[0 for _ in range(len(D_ij))] for _ in range(len(D_ij))]
+        for i in range(len(D_ij)):
+            for j in range(len(D_ij)):
+                T_ij[i][j] = (timedelta(hours=(D_ij[i][j] / speed)).total_seconds() / 60)
+        print("T", T_ij)
 
-        D_ij = haversine(lat_lon, lat_lon) * 6371
-
-        # Travel time matrix
-        speed = 20
-
-        T_ij = np.empty(
-            shape=(self.num_nodes_and_depots,
-                   self.num_nodes_and_depots), dtype=timedelta
-        )
-
-        for i in range(self.num_nodes_and_depots):
-            for j in range(self.num_nodes_and_depots):
-                T_ij[i][j] = (
-                    timedelta(hours=(D_ij[i][j] / speed)
-                              ).total_seconds()
-                )
-
-        # rush hour modelling:
-        if not (df.iloc[0]["Requested Pickup Time"].weekday() == 5):
-            for k in range(self.n):
-                for l in range(self.n):
-                    if df.iloc[k]["Requested Pickup Time"].hour >= 15 and df.iloc[k]["Requested Pickup Time"].hour < 17 and df.iloc[l]["Requested Pickup Time"].hour >= 15 and df.iloc[l]["Requested Pickup Time"].hour < 17:
-                        T_ij[k][l] = T_ij[k][l]*R_F
-                        T_ij[k+self.n][l] = T_ij[k+self.n][l]*R_F
-                        T_ij[k][l+self.n] = T_ij[k][l+self.n]*R_F
-                        T_ij[k+self.n][l+self.n] = T_ij[k+self.n][l+self.n]*R_F
+        # Rush hour modelling:
+        for k in range(len(T_ij)):
+            average_start_time = df.iloc[k][""]
+            if "starting time is between 7 or 9"
+            if "starting time is between 15 and 17"
+            df.iloc[k]["Requested Pickup Time"].hour >= 15 and df.iloc[k]["Requested Pickup Time"].hour < 17 and df.iloc[l]["Requested Pickup Time"].hour >= 15 and df.iloc[l]["Requested Pickup Time"].hour < 17:
+                T_ij[k][l] = T_ij[k][l]*rush_factor
+                T_ij[k+self.n][l] = T_ij[k+self.n][l]*rush_factor
+                T_ij[k][l+self.n] = T_ij[k][l+self.n]*rush_factor
+                T_ij[k+self.n][l+self.n] = T_ij[k+self.n][l+self.n]*rush_factor
+               
 
         return T_ij
 
-T_ij = travel_matrix(df_nodes)
-print("Dette er er matrisa:", T_ij)
+matrisa = travel_matrix(df_nodes)
+print("Dette er er matrisa:", matrisa)
