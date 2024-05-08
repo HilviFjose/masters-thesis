7e2109bf51a132f731cc46d79c9ad71c07c29723
Agnes <you@example.com>
2024-05-08 21:02:36 +0200

    Klar for kjore

--- config/main_config.py
+++ config/main_config.py
@@ -8,7 +8,7 @@ import math
 reaction_factor = 0.7
 
 # Iterations in ALNS
-iterations = 20
+iterations = 400
 
 # Requirement for how good a candidate must be before doing the local search. -- TODO: these must be tuned
 local_search_req = 0.02
@@ -62,7 +62,7 @@ days = 5
 depot = (59.9365, 10.7396)
 
 #Number of constructed solutions 
-num_of_constructions = 1  #OBS: Bør ikke settes til over 5, for er usikkert hvro mye prosessoren tåler
+num_of_constructions = 10  #OBS: Bør ikke settes til over 5, for er usikkert hvro mye prosessoren tåler
 
 #Insertor choises [0,1,2,3, 4] for [simple, better with limited regret 1, better sith limited regeret 2, better, best ]
 construction_insertor = 2 #W
@@ -70,14 +70,14 @@ repair_insertor = 1
 illegal_repair_insertor = 2
 
 
-max_num_regret1 = 20
+max_num_regret1 = 50
 max_num_regret2 = 100
 
 #Insertor som kan brukes en andel av gangene 
 fraction_repair_insertor = 1 
-frequecy_of_fraction_insertion = 0.5
+frequecy_of_fraction_insertion = 0.01
 modNum_for_fraction_insertion = math.ceil(iterations*frequecy_of_fraction_insertion) 
 
 
 #Number of paralell processes 
-num_of_paralell_iterations = 1
+num_of_paralell_iterations = 3
