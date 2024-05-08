43ecad7ef287b1340454d06001f1cfe3977be0b9
hilvifjose <hilfjo@gmail.com>
2024-05-08 19:35:01 +0200

    bare noe recovery saker

--- config/main_config.py
+++ config/main_config.py
@@ -8,7 +8,7 @@ import math
 reaction_factor = 0.7
 
 # Iterations in ALNS
-iterations = 400
+iterations = 10
 
 # Requirement for how good a candidate must be before doing the local search. -- TODO: these must be tuned
 local_search_req = 0.02
